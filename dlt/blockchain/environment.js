fba9612cd9329fb4b8c71aa48e42784fedc86604
Thuan Nguyen <thuan_nguyen@unitedsolutions.biz>
2019-10-02 23:10:09 -0400

    Update environment.js

--- dlt/blockchain/environment.js
+++ dlt/blockchain/environment.js
@@ -1,11 +1,11 @@
 //IP address for the DLT
 const settings = {
     DLT_IP: '172.31.66.248',
-    PEERICA: 'ica.peer.hhs.gov',
-    PEERORG: 'peer.hhs.gov',
-    ORDERERORG: 'orderer.hhs.gov',
-    PEER: 'peer1.peer.hhs.gov',
-    ORDERER: 'orderer1.orderer.hhs.gov'
+    PEERICA: 'ica.peer.unitedsolutions.biz',
+    PEERORG: 'peer.unitedsolutions.biz',
+    ORDERERORG: 'orderer.unitedsolutions.biz',
+    PEER: 'peer1.peer.unitedsolutions.biz',
+    ORDERER: 'orderer1.orderer.unitedsolutions.biz'
 }
 
 module.exports.settings = settings;
